3651a77054b328a4d7a14dd385bd0695842af536
kceron <karemiceron@gmail.com>
2020-12-30 08:32:10 -0500

    bonus

--- 04-data.js
+++ 04-data.js
@@ -19,11 +19,26 @@ console.log(sum(range(1, 10)));
 // → 55
 
 // 3.  As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
-// // Your code here.
+// Your code here.
+function range(start, end, step = 1) {
+  let arr = [];
+  let stepVal;
+  if ((step = 1)) {
+    stepVal = 1;
+    for (let i = start; i <= end; i++) {
+      arr.push(i);
+    }
+    return arr;
+  } else {
+    stepVal = step;
+    for (let i = start; i <= end; i++) {
+      arr.push(i + stepVal);
+    }
+    return arr;
+  }
+}
+// console.log(range(1,10,2))
+// [1,3,5,7,9]
 
-// console.log(range(1, 10));
-// // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // console.log(range(5, 2, -1));
-// // → [5, 4, 3, 2]
-// console.log(sum(range(1, 10)));
-// // → 55
+// → [5, 4, 3, 2]
