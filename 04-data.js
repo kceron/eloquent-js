0477e29b8c28a971d951efce50a5e5a771376bdc
kceron <karemiceron@gmail.com>
2020-12-29 23:11:58 -0500

    sum

--- 04-data.js
+++ 04-data.js
@@ -11,6 +11,12 @@ console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // 2.  Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
+// Your code here
+function sum(numsArr) {
+  return numsArr.reduce((acumulator, currentVal) => acumulator + currentVal);
+}
+console.log(sum(range(1, 10)));
+// → 55
 
 // 3.  As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
 // // Your code here.
