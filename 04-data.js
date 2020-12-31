5aea9dea70f588361d3d6198eb61cb3c7102500a
kceron <karemiceron@gmail.com>
2020-12-30 22:50:29 -0500

    rev arr

--- 04-data.js
+++ 04-data.js
@@ -37,3 +37,19 @@ function range(start, end, step = 1) {
 // [1,3,5,7,9]
 // console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
+
+// For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method.
+
+// Your code here.
+function reverseArray(arr) {
+  let newArr = [];
+  for (let i = arr.length - 1; i > 0; i--) {}
+}
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+
+let arrayValue = [1, 2, 3, 4, 5];
+function reverseArrayInPlace(arr) {}
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
