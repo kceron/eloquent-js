e2efc2f7e9e9cb39e84b05cfcc480912d3c94c6c
kceron <karemiceron@gmail.com>
2021-02-15 20:13:45 -0500

    flattened arr

--- 05-higher-order.js
+++ 05-higher-order.js
@@ -1,6 +1,12 @@
 // Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
+arrays.reduce(
+  function(a, b) {
+    return a.concat(b);
+  },
+  []
+);
 // → [1, 2, 3, 4, 5, 6]
 
 // Write a higher-order function loop that provides something like a for loop statement. It takes a value, a test function, an update function, and a body function. Each iteration, it first runs the test function on the current loop value and stops if that returns false. Then it calls the body function, giving it the current value. Finally, it calls the update function to create a new value and starts from the beginning.
