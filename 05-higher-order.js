c23dab30a7000e607a6f56e35c4254860df5f15a
kceron <karemiceron@gmail.com>
2021-02-15 20:29:17 -0500

    everything

--- 05-higher-order.js
+++ 05-higher-order.js
@@ -23,3 +23,23 @@ loop(3, n => n > 0, n => n - 1, console.log);
 // → 2
 // → 1
 
+// Analogous to the some method, arrays also have an every method. This one returns true when the given function returns true for every element in the array. In a way, some is a version of the || operator that acts on arrays, and every is like the && operator.
+// Implement every as a function that takes an array and a predicate function as parameters. Write two versions, one using a loop and one using the some method.
+function every(array, predicate) {
+  // Your code here.
+    for (let element of array) {
+    if (!predicate(element)) return false;
+  }
+  return true;
+}
+
+function every2(array, predicate) {
+  return !array.some(element => !predicate(element));
+}
+
+// console.log(every([1, 3, 5], n => n < 10));
+// // → true
+// console.log(every([2, 4, 16], n => n < 10));
+// // → false
+// console.log(every([], n => n < 10));
+// // → true
