8365bccbcc7ac9dfffa9dcb68dc91c3c9513af85
kceron <karemiceron@gmail.com>
2021-02-15 20:16:38 -0500

    flattened arr

--- 05-higher-order.js
+++ 05-higher-order.js
@@ -8,6 +8,8 @@ arrays.reduce(
   []
 );
 // → [1, 2, 3, 4, 5, 6]
+// another way
+// console.log(arrays.reduce((flat, current) => flat.concat(current), []));
 
 // Write a higher-order function loop that provides something like a for loop statement. It takes a value, a test function, an update function, and a body function. Each iteration, it first runs the test function on the current loop value and stops if that returns false. Then it calls the body function, giving it the current value. Finally, it calls the update function to create a new value and starts from the beginning.
 // Your code here.
